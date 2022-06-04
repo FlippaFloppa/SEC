e0391382a8271eeaaf05cdad8eb6be11bfbbd925
BlessedRebuS <patrick.difazio@studio.unibo.it>
2022-06-04 18:51:32 +0200

    aggiunto bof finder

---
+++ bof/bufferfinder.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+tmp=tmp$$.txt
+
+if [ $# -ne 1 ]
+then
+	echo Usage: ./bufferFinder.sh nomefile
+	exit
+fi
+
+for i in {1..4096}
+do
+	#to speed up i do it every 32 bytes
+	if [ `expr $i % 32` -eq 0 ]
+	then
+		echo provo con $i
+		error=$($1 $(python -c "print('A'*$i)") 2>&1 1>$tmp); exit=$?;
+		if [ $exit -ne 0 ]
+		then 
+			echo Va in overflow da $i
+			num=`expr $i - 32`
+			for j in $(seq $num $i)
+			do	
+				echo provo con $j
+				error=$($1 $(python -c "print('A'*$j)") 2>&1 1>$tmp); exit=$?;
+				if [ $exit -ne 0 ]
+				then
+					rm $tmp; echo Va in overflow da $j; exit;
+				fi
+			done
+		fi
+	fi
+done
+
